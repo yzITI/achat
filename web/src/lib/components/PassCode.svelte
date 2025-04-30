6834bc50e4bf10bacd4885a1bedcc2fecd868b41
Phantomlsh <30175757+phantomlsh@users.noreply.github.com>
2025-04-29 19:40:08 -0500

    Add PassCode help

--- web/src/lib/components/PassCode.svelte
+++ web/src/lib/components/PassCode.svelte
@@ -1,11 +1,11 @@
 <script>
-  import { AIcon } from 'ace.svelte'
-  import { mdiLock, mdiArrowRight } from '@mdi/js'
+  import { AIcon, AExpand } from 'ace.svelte'
+  import { mdiLock, mdiArrowRight, mdiHelp } from '@mdi/js'
   import { sha256 } from '$lib/utilities/crypto.js'
   import { S, LS } from '$lib/S.svelte'
   import { handshake } from '$lib/C.js'
 
-  let passcode = $state(''), nickname = $state('')
+  let passcode = $state(''), nickname = $state(''), showHelp = $state(false)
   async function enter () {
     S.token = await sha256(passcode)
     LS.token = S.token
@@ -20,11 +20,24 @@
   <div class="p-6 rounded-xl flex flex-col justify-center items-center">
     <AIcon path={mdiLock} size="5rem"/>
     <h1 class="text-4xl font-bold my-4">PassCode</h1>
-    <input type="password" class="w-80 px-4 py-2 my-2 bg-neutral-700 rounded outline-none text-lg text-center transition-all focus:bg-neutral-600" placeholder="Enter your PassCode" bind:value={passcode} />
+    <div class="relative flex items-center overflow-hidden">
+      <input type="password" class="w-80 px-4 py-2 my-2 bg-neutral-700 rounded outline-none text-lg text-center transition-all focus:bg-neutral-600" placeholder="Enter your PassCode" bind:value={passcode} />
+      <button class="absolute rounded-full p-1 bg-neutral-500 cursor-pointer transition-all duration-500 { showHelp ? '-right-8' : 'right-2' }" onclick={() => showHelp = true}>
+        <AIcon path={mdiHelp} size="1.25rem"></AIcon>
+      </button>
+    </div>
     <input type="text" class="w-80 px-4 py-2 my-2 bg-neutral-700 rounded outline-none text-lg text-center transition-all focus:bg-neutral-600" placeholder="Nickname" bind:value={nickname} />
     <button class="bg-white transition-all duration-500 rounded-full text-black flex items-center justify-end h-12 w-12 my-4 hover:w-32 cursor-pointer overflow-hidden" onclick={enter}>
       <b class="mx-3 text-lg">Enter</b>
       <AIcon path={mdiArrowRight} size="2rem" class="mx-2"></AIcon>
     </button>
+    <AExpand show={showHelp}>
+      <div class="w-80 flex flex-col items-start text-sm">
+        <h3 class="font-bold text-lg">What's PassCode?</h3>
+        <p>PassCode is your identity and permission in AChat. Think about it as username + password.</p>
+        <p>Your passcode should be something only you know (secret) and something others would not use (unique). <b>Don't forget it!</b></p>
+        <p class="my-2"><span class="italic">What the observer knows is inseparable from what the observer is.</span> (Wojciech H. Zurek)</p>
+      </div>
+    </AExpand>
   </div>
 </div>
