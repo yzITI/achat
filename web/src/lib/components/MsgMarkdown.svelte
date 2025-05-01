f76c94fe9266a691e26ad6499a095cfe7d3d3481
Phantomlsh <30175757+phantomlsh@users.noreply.github.com>
2025-05-01 08:47:52 -0500

    Add katex

--- web/src/lib/components/MsgMarkdown.svelte
+++ web/src/lib/components/MsgMarkdown.svelte
@@ -2,9 +2,11 @@
   import { marked } from 'marked'
   import DOMPurify from 'dompurify'
   const { msg } = $props()
+  let el = $state()
   const html = DOMPurify.sanitize(marked.parse(msg.content, { breaks: true }))
+  setTimeout(() => window.renderMathInElement(el))
 </script>
 
-<div>
+<div class="prose prose-zinc prose-invert" bind:this={el}>
   {@html html}
 </div>
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -14,6 +14,12 @@
   if (data.token) S.token = data.token
 </script>
 
+<svelte:head>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.22/dist/katex.min.css" integrity="sha384-5TcZemv2l/9On385z///+d7MSYlvIEw9FuZTIdZ14vJLqWphw7e7ZPuOiCHJcFCP" crossorigin="anonymous">
+  <script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.22/dist/katex.min.js" integrity="sha384-cMkvdD8LoxVzGF/RPUKAcvmm49FQ0oxwDF3BGKtDXcEc+T1b2N+teh/OJfpU0jr6" crossorigin="anonymous"></script>
+  <script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.22/dist/contrib/auto-render.min.js" integrity="sha384-hCXGrW6PitJEwbkoStFjeJxv+fSOOQKOPbJxSfM6G5sWZjAyWhXiTIIAmQqnlLlh" crossorigin="anonymous"></script>
+</svelte:head>
+
 <div class="w-full h-dvh overflow-hidden bg-black text-white">
   {#if !S.token}
     <div transition:fade class="fixed w-full h-full top-0 left-0 z-50">
@@ -21,7 +27,7 @@
     </div>
   {/if}
   <div class="flex h-full">
-    <div class="w-80 h-full top-0 fixed z-40 md:relative transition-all duration-300 { S.showChannel ? 'left-0' : '-left-96 md:left-0' }" style="min-width: 20rem;">
+    <div class="w-80 h-full top-0 fixed z-40 md:relative transition-all duration-300 { S.showChannel ? 'left-0' : '-left-80 md:left-0' }" style="min-width: 20rem;">
       <div class="h-16">
         <Status />
       </div>
