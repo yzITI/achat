17524f6a0fa1535e200299c39433722a41e7ac1c
Phantomlsh <30175757+phantomlsh@users.noreply.github.com>
2025-05-03 11:27:49 -0500

    Fix index

--- index.js
+++ index.js
@@ -2,11 +2,10 @@
 console.log('Starting core #...')
 import './core/index.js'
 
-// AChat Web
-app.use(express.static('./web/build/'))
-
 // AChat File Server
 import express from 'express'
 const app = express()
 app.listen(8081, () => { console.log('> File server listening on port 8081') })
 
+// AChat Web
+app.use(express.static('./web/build/'))
